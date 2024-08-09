b3cdb82f92cf2d688288ba9da62eadcdd85b890e
Alan Raff <alanr917@hotmail.com>
2024-08-09 15:32:58 -0400

    reorder functions

--- web.py
+++ web.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 video_stream = VideoCamera()
 
-@app.route("/")
-def index():
-	return render_template("index.html")
-
 def gen(camera):
 	while True:
 		frame = camera.get_frame()
 		yield (b"--frame\r\n"
 			   b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+
+@app.route("/")
+def index():
+	return render_template("index.html")
 
 @app.route("/video_feed")
 def video_feed():
