7343e452753b9502dd3ed9980005c8c72fe7f6b9
Alan Raff <alanr917@hotmail.com>
2024-08-10 11:58:21 -0400

    add route to drop treat

--- web.py
+++ web.py
@@ -1,15 +1,19 @@
 import os
+import pigpio
 from dotenv import load_dotenv
-from flask import Flask, jsonify, render_template, Response
+from flask import Flask, redirect, render_template, Response
 from flask_basicauth import BasicAuth
 from webcam import VideoCamera
 
 load_dotenv()
 app = Flask(__name__)
-app.config['BASIC_AUTH_USERNAME'] = os.getenv("USERNAME")
-app.config['BASIC_AUTH_PASSWORD'] = os.getenv("PASSWORD")
+app.config["BASIC_AUTH_USERNAME"] = os.getenv("USERNAME")
+app.config["BASIC_AUTH_PASSWORD"] = os.getenv("PASSWORD")
 basic_auth = BasicAuth(app)
 video_stream = VideoCamera()
+servo_pin = 17
+pi = pigpio.pi()
+pi.set_mode(servo_pin, pigpio.OUTPUT)
 
 def gen(camera):
 	while True:
@@ -26,6 +30,19 @@ def index():
 @basic_auth.required
 def video_feed():
 	return Response(gen(video_stream), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+@app.route("/drop_treat")
+@basic_auth.required
+def drop_treat():
+	pi.set_servo_pulsewidth(servo_pin, 1500) #stop
+	time.sleep(0.5)
+	pi.set_servo_pulsewidth(servo_pin, 1560) #spin
+	time.sleep(0.25)
+	pi.set_servo_pulsewidth(servo_pin, 1500) #stop
+	time.sleep(0.5)
+	pi.stop()
+	return redirect("/")
+
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port="3000")
