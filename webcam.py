1a5ef9556aa64c50da0e3978406eeacca080d57e
Alan Raff <alanr917@hotmail.com>
2024-08-09 23:17:53 -0400

    remove whitespace

--- webcam.py
+++ webcam.py
@@ -5,7 +5,7 @@ class VideoCamera(object):
 		self.video = cv2.VideoCapture(0)
 
 	def __del__(self):
-		self.video.release()        
+		self.video.release()
 
 	def get_frame(self):
 		ret, frame = self.video.read()
