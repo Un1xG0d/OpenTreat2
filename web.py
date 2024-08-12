42e501b351ab0b762749f6c98953f591c1ac3167
Alan Raff <alanr917@hotmail.com>
2024-08-11 22:27:32 -0400

    add try statement for camera frame functions

--- web.py
+++ web.py
@@ -18,9 +18,12 @@ pi.set_mode(servo_pin, pigpio.OUTPUT)
 
 def gen(camera):
 	while True:
-		frame = camera.get_frame()
-		yield (b"--frame\r\n"
-			   b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+		try:
+			frame = camera.get_frame()
+			yield (b"--frame\r\n"
+				   b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+		except Exception as e:
+			print(e)
 
 @app.route("/")
 @basic_auth.required
